3130d6be0e0998370c0486c5f5ced6633ee0bfbb
Wenlei Xie <wenlei.xie@gmail.com>
2019-10-11 12:51:43 -0700

    Rename package name to com.facebook.airlift

--- src/test/java/com/facebook/airlift/testing/postgresql/TestTestingPostgreSqlServer.java
+++ src/test/java/com/facebook/airlift/testing/postgresql/TestTestingPostgreSqlServer.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.airlift.testing.postgresql;
+package com.facebook.airlift.testing.postgresql;
 
 import org.testng.annotations.Test;
 
