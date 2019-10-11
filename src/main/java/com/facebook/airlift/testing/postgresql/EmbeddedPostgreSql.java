9d8424586d302e214ae89df3b755f630d6b6a8bc
Wenlei Xie <wenlei.xie@gmail.com>
2019-10-11 12:51:43 -0700

    Rename package name to com.facebook.airlift

--- src/main/java/com/facebook/airlift/testing/postgresql/EmbeddedPostgreSql.java
+++ src/main/java/com/facebook/airlift/testing/postgresql/EmbeddedPostgreSql.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.airlift.testing.postgresql;
+package com.facebook.airlift.testing.postgresql;
 
 import com.facebook.airlift.log.Logger;
 import com.google.common.collect.ImmutableMap;
